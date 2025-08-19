3819cc8c836d9fd25c132d75348dd6b404ac6c5e
shivani kumari <157093125+shivanikumari12345678@users.noreply.github.com>
2025-08-19 13:35:46 +0530

    database connected

--- index.js
+++ index.js
@@ -1,79 +1,104 @@
 import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-dotenv.config()
-const app = express()
-app.use(cors())
-let notes = [
-  {
-    id: '1',
-    content: 'HTML is easy',
-    important: true,
-  },
-  {
-    id: '2',
-    content: 'Browser can execute only JavaScript',
-    important: false,
-  },
-  {
-    id: '3',
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true,
-  },
-]
 
-app.use(express.json())
+import Note from './models/note.js'
+
+const app = express()
+
+let notes = []
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
 app.use(express.static('dist'))
+app.use(express.json())
+app.use(requestLogger)
+
+
+
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find((note) => note.id === id)
-
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
+app.get('/api/notes/:id', (request, response,next) => {
+  Note.findById(request.params.id)
+  .then(note=>{
+    if(note){
+      return response.json(note)
+    }else{
+      return response.status(404).end()
+    }})
+  .catch((e)=>next(e))
 })
 
-const generateId = () => {
-  const maxId =
-    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0
-  return String(maxId + 1)
-}
 
-app.post('/api/notes', (request, response) => {
+
+app.post('/api/notes', (request, response,next) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing',
-    })
-  }
+  if (!body.content) { return response.status(400).json({error: 'content missing',})}
 
-  const note = {
+  const note = new Note({
     content: body.content,
-    important: body.important || false,
-    id: generateId(),
-  }
+    important: body.important || false
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote=>
+    response.json(savedNote)
+  )
+  .catch(e=>next(e))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter((note) => note.id !== id)
+app.put('/api/notes/:id',(request,response,next)=>{
+  const {content,important}=request.body
+  Note.findById(request.params.id)
+  .then(note=>{
+    if(!note) return response.status(404).end()
+    note.content=content
+    note.important=important
+    return note.save().then((updatedNote)=>response.json(updatedNote))
+  })
+  .catch((e)=>next(e))
+})
 
-  response.status(204).end()
+app.delete('/api/notes/:id', (request, response,next) => {
+  Note.findByIdAndDelete('request.params.id')
+  .then(result=>response.status(204).end())
+  .catch((e)=>next(e))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }else if(error.name === 'ValidationError'){
+    return response.status(400).send({error:error.message})
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
+
+app.use((error, request, response, next) => {
+  return response.status(500).json({ error: 'Something went wrong' })
 })
 
 const PORT =  process.env.PORT || 3000;
---
+++ mongo.js
@@ -0,0 +1,28 @@
+
+
+import mongoose from "mongoose"
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://shivani2:${password}@cluster0.o2cv4h5.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
